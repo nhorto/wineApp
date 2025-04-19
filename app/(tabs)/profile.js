a8e6296a3f438f824461f1ac5f47c03cef667586
nhorto <155685655+nhorto@users.noreply.github.com>
2025-04-19 14:43:57 -0400

    Update profile.js

---
+++ app/(tabs)/profile.js
@@ -0,0 +1,11 @@
+// app/(tabs)/profile.js
+
+import { View, Text } from 'react-native';
+
+export default function ProfileScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Profile Screen Placeholder</Text>
+    </View>
+  );
+}
